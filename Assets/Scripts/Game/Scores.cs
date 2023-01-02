6a0203a1075fb41d3d364729397888d6a580eddb
Trustedaid <92570103+Trustedaid@users.noreply.github.com>
2023-01-03 00:35:41 +0300

    Best Scores

--- Assets/Scripts/Game/Scores.cs
+++ Assets/Scripts/Game/Scores.cs
@@ -1,31 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class BestScoreData
+{
+    public int score = 0;
+}
 public class Scores : MonoBehaviour
 {
     public Text scoreText;
+    private bool newBestScore_ = false;
+    private BestScoreData bestScores_ = new BestScoreData();
     private int currentScores_;
 
+    private string bestScoreKey_ = "bsdat";
+    private void Awake()
+    {
+        if (BinaryDataStream.Exist(bestScoreKey_))
+        {
+            StartCoroutine(ReadDataFile());
+        }
+    }
+    private IEnumerator ReadDataFile()
+    {
+        bestScores_ = BinaryDataStream.Read<BestScoreData>(bestScoreKey_);
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Read Best Scores = " + bestScores_.score);
+    }
     private void Start()
     {
         currentScores_ = 0;
+        newBestScore_ = false;
         UpdateScoreText();
     }
 
     private void OnEnable()
     {
         GameEvents.AddScores += AddScores;
+        GameEvents.GameOver += SaveBestScores;
     }
 
     private void OnDisable()
     {
         GameEvents.AddScores -= AddScores;
+        GameEvents.GameOver -= SaveBestScores;
     }
+
+    private void SaveBestScores(bool newBestScores)
+    {
+        BinaryDataStream.Save<BestScoreData>(bestScores_,bestScoreKey_);
+    }
+
     private void AddScores(int scores)
     {
         currentScores_ += scores;
+        if (currentScores_ > bestScores_.score)
+        {
+            newBestScore_ = true;
+            bestScores_.score = currentScores_;
+        }
+
         UpdateScoreText();
     }
     private void UpdateScoreText()
